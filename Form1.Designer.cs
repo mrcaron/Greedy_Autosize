e4744347d5524ca8aa398745165a4625f357b71c
Michael Caron <michael.r.caron@gmail.com>
2011-07-18 09:46:36 -0500

    added code

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace AutosizeNightmare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelSizeBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gbSizeBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.Window;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 48);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(578, 383);
+            this.flowLayoutPanel1.TabIndex = 0;
+            this.flowLayoutPanel1.Resize += new System.EventHandler(this.flowLayoutPanel1_Resize);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Panel Size:";
+            // 
+            // panelSizeBox
+            // 
+            this.panelSizeBox.Location = new System.Drawing.Point(85, 5);
+            this.panelSizeBox.Name = "panelSizeBox";
+            this.panelSizeBox.ReadOnly = true;
+            this.panelSizeBox.Size = new System.Drawing.Size(204, 20);
+            this.panelSizeBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(295, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "GB Size:";
+            // 
+            // gbSizeBox
+            // 
+            this.gbSizeBox.Location = new System.Drawing.Point(349, 5);
+            this.gbSizeBox.Name = "gbSizeBox";
+            this.gbSizeBox.ReadOnly = true;
+            this.gbSizeBox.Size = new System.Drawing.Size(242, 20);
+            this.gbSizeBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 443);
+            this.Controls.Add(this.gbSizeBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panelSizeBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox panelSizeBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox gbSizeBox;
+
+
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AutosizeNightmare
+{
+    public partial class Form1 : Form
+    {
+        List<GroupBox> gbs = new List<GroupBox>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 4; i++)
+            {
+                GroupBox b = createCustomGB(""+i);
+                gbs.Add(b);
+                flowLayoutPanel1.Controls.Add(b);
+            }
+
+            /*
+            groupBox1.MinimumSize = new Size(
+                flowLayoutPanel1.Size.Width - flowLayoutPanel1.Margin.Right - 10,
+                0                                           
+                );
+             */
+        }
+
+        private void flowLayoutPanel1_Resize(object sender, EventArgs e)
+        {
+            foreach (GroupBox g in gbs)
+            {
+                g.MinimumSize = new Size(
+                    flowLayoutPanel1.Size.Width - flowLayoutPanel1.Margin.Right - 10,
+                    0
+                );
+            }
+        }
+
+        private GroupBox createCustomGB(String name)
+        {
+            GroupBox gb = new GroupBox();
+            gb.AutoSize = true;
+            gb.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            gb.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            
+            FlowLayoutPanel p = new FlowLayoutPanel();
+            p.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            p.AutoSize = true;
+            p.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            p.WrapContents = true;
+            p.Location = new System.Drawing.Point(6, 19);
+
+            gb.Controls.Add(p);
+            gb.Text = String.Format("Group {0}", name);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Button b = new Button();
+                b.Text = String.Format("Button {0}", i);
+                p.Controls.Add(b);
+            }
+                        
+            gb.MinimumSize = new Size(
+                flowLayoutPanel1.Size.Width - flowLayoutPanel1.Margin.Right - 10,
+                0
+            );
+
+            return gb;
+        }
+    }
+}
